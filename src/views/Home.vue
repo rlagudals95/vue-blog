d71e9d18283a194fe2fa031fe9986f8e316d7e6c
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-08-01 01:40:40 +0900

    props오류

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="home">
-    <BlogPost :post="welcomeScreen"/>
-    <BlogPost :post="post"  v-for="(post, index) in sampleBlogPost" :key="index" />
+    <BlogPost :post="welcomeScreen" />
+    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" /> 
   </div>
 </template>
 
 <script>
-import BlogPost from '../conponents/BlogPost'
-
+import BlogPost from '../conponents/BlogPost';
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: { BlogPost },
   data(){
     return{
       welcomeScreen: {
-        title: 'welcome',
+        title: 'Welcome',
         blogPost: 'Developer HMK1022',
         welcomeScreen: true,
-        photo: 'coding'
+        photo: 'coding',
       },
+      // 위가 먹히질 않는다..후.,.. ㅅㅂ/...
       sampleBlogPost : [
         {
-          title: "This is a Filter Title!",
-          blogHTML: '항해중인 개발자',
-          blogCoverPhoto: 'photo'
-
+          title: "Welcome",
+          blogPost: 'Developer HMK1022 Developer HMK1022 Developer HMK1022 Developer HMK1022',
+          welcomeScreen: true,
+          blogCoverPhoto: 'coding'
         },
         {
-         title: "This is a Filter Title2!",
-          blogHTML: '항해중인 개발자',
-          blogCoverPhoto: 'photo'
-        }
-      ]
-      
-
-    }
-  }
+          title: "This is a Filter Title2!",
+          blogHTML: '항해중인 개발자2',
+          blogCoverPhoto: 'designed-for-everyone'
+        },
+      ],
+    };
+  },
 };
 </script>
