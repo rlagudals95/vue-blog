7961dde870e5b3be16532a4795bdab0f15d85a4d
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-08-01 23:38:30 +0900

    blogCards

--- src/App.vue
+++ src/App.vue
@@ -68,6 +68,32 @@ export default {
     path {
       fill:#fff;
     }
- 
+}
+
+.blog-card-wrap {
+  position: relative;
+  padding: 80px 16px;
+  background-color: #f1f1f1;
+  @media (min-width: 500px){
+    padding: 100px 16px;
+  }
+
+  .blog-cards {
+    display: grid;
+    gap: 32px;
+    grid-template-columns: repeat(4,1fr);
+       
+    @media(max-width: 500px) { // 500px 밑으로 내려가면
+  
+      grid-template-columns: repeat(2,1fr);
+    }
+    @media(max-width: 900px) {
+      grid-template-columns: repeat(3,1fr);
+    }
+    @media(max-width: 1200px) {
+     
+      grid-template-columns: repeat(4,1fr);
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,23 +2,32 @@
   <div class="home">
     <BlogPost :post="welcomeScreen" />
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" /> 
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards :post="post" v-for="(post, index) in sampleBlogCards " :key= "index"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from '../conponents/BlogPost';
+import BlogCards from "../conponents/BlogCard"
 export default {
   name: "Home",
-  components: { BlogPost },
+  components: { BlogPost, BlogCards },
   data(){
     return{
-      welcomeScreen: {
-        title: 'Welcome',
-        blogPost: 'Developer HMK1022',
-        welcomeScreen: true,
-        photo: 'coding',
-      },
-      // 위가 먹히질 않는다..후.,.. ㅅㅂ/...
+      // welcomeScreen: {
+      //   title: 'Welcome',
+      //   blogPost: 'Developer HMK1022',
+      //   welcomeScreen: true,
+      //   photo: 'coding',
+      // },
+      // welcomeScreen 먹히질 않는다..후.,..
       sampleBlogPost : [
         {
           title: "Welcome",
@@ -31,8 +40,34 @@ export default {
           blogHTML: '항해중인 개발자2',
           blogCoverPhoto: 'designed-for-everyone'
         },
+          {
+          title: "This is a Filter Title2!",
+          blogHTML: '항해중인 개발자2',
+          blogCoverPhoto: 'beautiful-stories'
+        },
+      ],
+      sampleBlogCards : [
+        {blogTitle: "Blog Cards #1", blogCoverPhoto:"stock-1", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Cards #2", blogCoverPhoto:"stock-2", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Cards #3", blogCoverPhoto:"stock-3", blogDate: "May 1, 2021"},
+        {blogTitle: "Blog Cards #4", blogCoverPhoto:"stock-4", blogDate: "May 1, 2021"},
       ],
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+
+.blog-card-wrap{
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+  h4 {
+
+  }
+}
+
+</style>
