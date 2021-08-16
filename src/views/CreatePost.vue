56e6714439b7f278efeac9a80f42dbdf6c14d232
unknown <you@example.com>
2021-08-16 10:44:22 +0900

    editor

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -4,6 +4,36 @@
       <div :class="{ invisible: !error }" class="err-message">
         <p><span>Error:</span>{{ this.errorMsg }}</p>
       </div>
+      <div class="blog-info">
+        <input type="text" placeholder="Enter Blog Title" v-model="blogTitle" />
+        <div class="upload-file">
+          <label for="blog-photo">Upload Cover Photo</label>
+          <input
+            type="file"
+            ref="blogPhoto"
+            id="blog-photo"
+            accept=".png , .jpg, .jpeg"
+          />
+          <button
+            class="preview"
+            :class="{ 'button-inactive': !this.$store.state.blogPhotoFileURL }"
+          >
+            Preview Photo
+          </button>
+          <span>File Chosen : {{ this.$store.state.blogPhotoName }}</span>
+        </div>
+      </div>
+      <div class="editor">
+        <vue-editor
+          :editorOptions="editorSettings"
+          v-model="blogHTML"
+          useCustomImageHandler
+        />
+      </div>
+      <div class="blog-actions">
+        <button>Publish Blog</button>
+        <router-link class="router-button" to="#">Post Preview</router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -21,7 +51,7 @@ export default {
       errorMsg: null,
       editorSettings: {
         modules: {
-          ImageResize: {},
+          imageResize: {},
         },
       },
     };
